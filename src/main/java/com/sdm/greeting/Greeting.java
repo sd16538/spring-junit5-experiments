3424ea976eeb85c87f3dd8fc3f4c8ff45b69b59d
sd16538 <shuchita12@gmail.com>
2016-07-20 20:33:13 +1000

    Initial commit

---
+++ src/main/java/com/sdm/greeting/Greeting.java
@@ -0,0 +1,38 @@
+package com.sdm.greeting;
+
+import static org.apache.commons.lang3.builder.EqualsBuilder.reflectionEquals;
+import static org.apache.commons.lang3.builder.HashCodeBuilder.reflectionHashCode;
+import static org.apache.commons.lang3.builder.ToStringBuilder.reflectionToString;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return reflectionEquals(this, o);
+    }
+
+    @Override
+    public int hashCode() {
+        return reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return reflectionToString(this);
+    }
+}
---
+++ src/main/java/com/sdm/greeting/GreetingSpringBootApplication.java
@@ -0,0 +1,11 @@
+package com.sdm.greeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GreetingSpringBootApplication.class, args);
+    }
+}
